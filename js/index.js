9f7955e7ef594ff58f656760f8a9fc2b784c24b3
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-06-18 13:37:41 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,22 @@
-export * as UI from './ui/index.js';
-export * as utils from './utils/index.js';
+import Dialog from './ui/Dialog.js';
+import Toast from './ui/Toast.js';
+import Countdown from './ui/Countdown.js';
+
+import chunk from './utils/chunk.js';
+import escapeHTML from './utils/escapeHTML.js';
+import formatDate from './utils/formatDate.js';
+import optimizeImage from './utils/optimizeImage.js';
+import pickRandom from './utils/pickRandom.js';
+import startCountdown from './utils/startCountdown.js';
+
+export {
+  Dialog,
+  Toast,
+  Countdown,
+  chunk,
+  escapeHTML,
+  formatDate,
+  optimizeImage,
+  pickRandom,
+  startCoundown
+};
