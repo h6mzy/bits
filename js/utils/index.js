d913bf09db6c1dff1ed661e4ea85997efb769953
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-06-18 10:34:07 +0800

    Create index.js

---
+++ js/utils/index.js
@@ -0,0 +1,6 @@
+export { chunk } from './chunk.js';
+export { escapeHTML } from './escapeHTML.js';
+export { formatDate } from './formatDate.js';
+export { optimizeImage } from './optimizeImage.js';
+export { pickRandom } from './pickRandom.js';
+export { startCountdown } from './startCountdown.js';
