16e91550b3a2dbb5d51123f34587965d5ae2fa1e
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-08-05 15:23:18 +0800

    Update formatDate.js

--- js/utils/formatDate.js
+++ js/utils/formatDate.js
@@ -1,20 +1,20 @@
 export const formatDate = (value, opts = {}) => {
   const {
-    locale = 'en-US',
-    day = 'numeric',
-    month = 'short',
-    year = 'numeric',
+    locale = "en-US",
+    day,
+    month,
+    year,
     hour,
     minute,
     hour12 = true
   } = opts;
 
   return new Intl.DateTimeFormat(locale, {
-    day,
-    month,
-    year,
-    hour,
-    minute,
-    hour12
+    ...(day && { day }),
+    ...(month && { month }),
+    ...(year && { year }),
+    ...(hour && { hour }),
+    ...(minute && { minute }),
+    ...(hour && { hour12 })
   }).format(new Date(value));
-}
+};
