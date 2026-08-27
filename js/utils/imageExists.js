1820dae5ef572124c8066a10e08cc9ab80a56ce7
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-08-27 15:26:33 +0800

    Update imageExists.js

--- js/utils/imageExists.js
+++ js/utils/imageExists.js
@@ -1,4 +1,7 @@
-export function imageExists(url, fallback = 'https://cdn.jsdelivr.net/gh/h6mzy/bits@1.11.11/examples/img/placeholder.webp') {
+export function imageExists(
+  url,
+  fallback = 'https://cdn.jsdelivr.net/gh/h6mzy/bits@1.11.11/examples/img/placeholder.webp'
+) {
   return new Promise(resolve => {
     const img = new Image();
 
